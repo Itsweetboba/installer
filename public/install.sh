8f49e0939e0494b92d7327f5ce20fa9307b2c058
Itsweetboba <piethecakeop@gmail.com>
2026-01-11 09:26:47 +0700

    Update install.sh

--- public/install.sh
+++ public/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading installer..."
-curl -fsSL https://domain-kamu.vercel.app/ireng.sh -o /tmp/ireng.sh
+curl -fsSL https://installerpaii.vercel.app/ireng.sh -o /tmp/ireng.sh
 
 chmod +x /tmp/ireng.sh
 bash /tmp/ireng.sh
