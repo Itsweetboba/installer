e969b9ecf3751051153ef4dd8a5b806039f4df53
Itsweetboba <piethecakeop@gmail.com>
2026-01-11 09:27:24 +0700

    Update ireng.sh

--- public/ireng.sh
+++ public/ireng.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # ╔════════════════════════════════════════════════════════════════════╗
-# ║ 🛡️  PABLO×KUROZI PROTECTOR SYSTEM v1.3                            ║
-# ║ Proteksi Controller Admin hanya untuk ID tertentu + Restore       ║
+# ║ 🛡️  PaiiTheStory PROTECTOR SYSTEM v1.0                            ║
+# ║ Proteksi Controller Admin hanya untuk ID tertentu + Restore        ║
 # ╚════════════════════════════════════════════════════════════════════╝
 
 # Warna
@@ -12,7 +12,7 @@ CYAN="\033[1;36m"
 YELLOW="\033[1;33m"
 BLUE="\033[1;34m"
 RESET="\033[0m"
-VERSION="1.4"
+VERSION="1.0"
 
 clear
 echo -e "${CYAN}${BOLD}"
